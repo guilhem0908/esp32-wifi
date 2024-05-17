02d8a24ede3475942d176d657162c6d0adfcbec4
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-17 15:39:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import socket
 import os
+import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 port = int(os.getenv('PORT', 8080))
 s.bind(("0.0.0.0", port))
+
 s.listen(5)
+
+http_response = """\
+HTTP/1.1 200 OK
+
+Thank you for connecting
+"""
 
 while True:
 
     client, addr = s.accept()
 
-    print("Got a connection from %s" % str(addr))
+    print(f"Got a connection from {addr}")
 
     while True:
         content = client.recv(1024)
@@ -19,10 +27,9 @@ while True:
         if not content:
             break
         else:
-            print("Received data: %s" % content.decode('utf-8'))
-    
-        msg = 'Thank you for connecting' + "\r\n"
-        client.send(msg.encode('utf-8'))
+            print(f"Received data: {content.decode('utf-8')}")
+
+        client.send(http_response.encode('utf-8'))
 
     print("Closing connection")
     client.close()
