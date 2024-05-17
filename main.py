f82b6c7c8a1f5031c2ae1321a08dd5dfdd209f61
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-17 16:10:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 import socket
 
+# Create a socket object and set options
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+# Get the port from environment variables or use 8080 as default
 port = int(os.getenv('PORT', 8080))
-print(port)
+print(f'Server will start on port {port}')
 
+# Bind the socket to a public host and the specified port
 s.bind(("0.0.0.0", port))
 
+# Tell the socket to listen for incoming connections
 s.listen(5)
 
 http_response = """\
@@ -17,26 +21,34 @@ HTTP/1.1 200 OK
 Thank you for connecting
 """
 
+# Forever loop to handle incoming connections
 while True:
-
+    # Accept an incoming connection
     client, addr = s.accept()
 
     print(f"Got a connection from {addr}")
 
-    client.send(http_response.encode('utf-8'))
+    # Send HTTP response
+    client.send(http_response.encode('utf8'))
 
+    # Handle received data
     while True:
         try:
+            # Receive data from client
             content = client.recv(1024)
 
+            # If no data is received, break the loop and close the connection
             if not content:
                 break
             else:
-                print(f"Received data: {content.decode('utf-8')}")
+                # Print received data
+                print(f"Received data: {content.decode('utf8')}")
 
         except ConnectionResetError:
+            # In case of abrupt client disconnection
             print("Connection was reset by client")
             break
 
+    # Close the connection
     print("Closing connection")
     client.close()
