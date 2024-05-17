1a7cc06b0fad013187f4f202aa157d9b997082d7
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-17 15:04:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("", 80))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("0.0.0.0", 80))
 s.listen(5)
 
 while True:
