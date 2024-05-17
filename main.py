a35f2a3384b3e7d5167ff6d5aec6247c311f163f
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-17 14:36:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("0.0.0.0", 80)
+s.bind(("0.0.0.0", 80))
 s.listen(5)
 
 while True:
@@ -13,6 +13,11 @@ while True:
 
     while True:
         content = client.recv(32)
+
+        
+        if not content:
+            break
+
     
         msg = 'Thank you for connecting' + "\r\n"
         client.send(msg.encode('ascii'))
