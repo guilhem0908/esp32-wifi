8e55fa1a8ca8cfac04091c41665cf022c85b6742
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-17 16:36:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return Response("Thank you for connecting", mimetype='text/plain')
+    return Response("Thank you for connecting\r", mimetype='text/plain')
 
 
 if __name__ == "__main__":
