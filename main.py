d34d2d61a0293bb4a4f7789566ef3a67a948b1cb
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-17 15:36:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import socket
+import os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("0.0.0.0", 8080))
+port = int(os.getenv('PORT', 8080))
+s.bind(("0.0.0.0", port))
 s.listen(5)
 
 while True:
