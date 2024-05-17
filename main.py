417834c1b99b2f6b44f750e78be1d291856e8078
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-17 14:02:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("192.168.170.195", 8090))
+s.listen(5)
+
+while True:
+
+    client, addr = s.accept()
+
+    while True:
+        content = client.recv(32)
+
+        if len(content) == 0:
+            break
+
+        else:
+            print(content)
+
+    print("Closing connection")
+    client.close()
