fea6d9a65586c0a8676c15d72d217a9cb9a6ff7e
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-24 16:34:07 +0200

    Update main.py
    sz'-edtyui

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return Response("Thank you for connecting\r\n", mimetype='text/plain')
+    return Response("Merci de vous être connecté\r\n", mimetype='text/plain')
 
+@app.route("/button_pressed", methods=['POST'])
+def button_pressed():
+    action = request.form.get('action')
+    if action == 'descend':
+        # Gérer l'action de descente ici
+        print("Bouton appuyé: DESCENDRE")
+        return Response("Bouton appuyé: DESCENDRE", mimetype='text/plain')
+    return Response("Action invalide", mimetype='text/plain')
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 8080))
