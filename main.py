fadd0c164318dfa0cedd1ab0edfd798c5c1dd6f4
Guilhem <57716752+guilhem0908@users.noreply.github.com>
2024-05-27 14:47:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
 import os
-from flask import Flask, Response, request, jsonify
-import threading
-import time
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-volet_status = {"status": "unknown"}  # Variable globale pour stocker l'état du volet
+# Variable globale pour stocker l'état du volet
+volet_status = {"status": "unknown"}
 
 @app.route("/")
 def home():
-    return Response("Thank you for connecting\r\n", mimetype='text/plain')
+    return "Server is running."
+
+@app.route("/button_pressed", methods=['POST'])
+def button_pressed():
+    action = request.form.get('action')
+    # Mettre à jour le statut du volet selon l'action
+    if action in ["descend", "monte", "arret"]:
+        volet_status["status"] = action
+    return jsonify({"action": action})
 
 @app.route("/update_status", methods=['POST'])
 def update_status():
-    global volet_status
-    volet_status['status'] = request.form.get('status')
+    volet_status["status"] = request.form.get('status')
     return jsonify(volet_status)
 
-@app.route("/get_status")
+@app.route("/get_status", methods=['GET'])
 def get_status():
     return jsonify(volet_status)
 
-shutdown_event = threading.Event()
-
-def run_flask():
-    port = int(os.getenv('PORT', 8080))
-
-    def check_shutdown():
-        while not shutdown_event.is_set():
-            time.sleep(0.1)
-        os._exit(0)
-
-    check_thread = threading.Thread(target=check_shutdown)
-    check_thread.start()
-
-    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
-
 if __name__ == "__main__":
-    run_flask()
+    port = int(os.getenv('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
